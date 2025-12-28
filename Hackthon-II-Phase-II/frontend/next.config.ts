1161fd5b2c129164368444867d397a61edde6cd9
subhankaladi <subhankaladi@gmail.com>
2025-12-28 13:37:54 +0500

    Update next.config.ts

--- Hackthon-II-Phase-II/frontend/next.config.ts
+++ Hackthon-II-Phase-II/frontend/next.config.ts
@@ -2,6 +2,8 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {}, // ✅ DISABLE turbopack safely
+
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
